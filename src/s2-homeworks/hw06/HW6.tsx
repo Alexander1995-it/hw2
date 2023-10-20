f9c0119022dcdea9955d94b3df3ffc0a8bdb991a
Alexander1995-it <sany_nax@mail.ru>
2023-10-20 16:17:36 +0300

    added restore locale storage

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -18,7 +18,7 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
-        setValue('')
+        setValue(restoreState('hw6-editable-span-value', ''))
 
     }
 
