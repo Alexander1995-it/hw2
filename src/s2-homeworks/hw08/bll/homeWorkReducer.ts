dfc602a34128deebc8d9550c326e326a45059877
Alexander1995-it <sany_nax@mail.ru>
2023-10-23 20:27:55 +0300

    did homework №8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,26 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            if (action.payload === 'up') {
+                return [...state].sort((a: UserType, b: UserType) => {
+                    if (a.name < b.name) return -1;
+                    if (a.name > b.name) return 1;
+                    return 0;
+                })
+            } else if (action.payload === 'down') {
+                return [...state].sort((a: UserType, b: UserType) => {
+                    if (a.name > b.name) return -1;
+                    if (a.name < b.name) return 1;
+                    return 0;
+                })
+            }
+            return state
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(el => el.age >= 18)
         }
         default:
             return state
