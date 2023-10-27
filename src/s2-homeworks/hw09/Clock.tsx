b16dc2aa52314b850977b2b53c654bc3b95cd23a
Alexander1995-it <sany_nax@mail.ru>
2023-10-27 08:37:38 +0300

    fix stringTime

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -33,13 +33,13 @@ function Clock() {
 
     }
 
-    const stringTime = date.toLocaleTimeString() || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringTime = `${date.getHours().toString().length === 1 ? `0${date.getHours()}` : date.getHours()}:${date.getMinutes().toString().length === 1 ? `0${date.getMinutes()}` : date.getMinutes()}: ${date.getSeconds().toString().length === 1 ? `0${date.getSeconds()}` : date.getSeconds()} ` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = date.toLocaleDateString() || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = date.toLocaleString('en', {weekday: "long"}) || <br/>
     const stringMonth = date.toLocaleString('en', {month: "long"})  || <br/> // пишут студенты
-
+    console.log (date.getHours())
     return (
         <div className={s.clock}>
             <div
