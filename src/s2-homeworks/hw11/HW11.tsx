5647d5b731f5f78ee6d8467c2c4ba48b962f3700
Alexander1995-it <sany_nax@mail.ru>
2023-10-28 20:21:50 +0300

    did homework №11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -16,7 +16,13 @@ function HW11() {
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
     const change = (event: any, value: any) => {
-        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+        let currentValue = event.target.value
+        if(Array.isArray(currentValue))  {
+            setValue1(currentValue[0])
+            setValue2(currentValue[1])
+        } else {
+            setValue1(currentValue)
+        }
     }
 
     return (
@@ -29,16 +35,16 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
-                            // сделать так чтоб value1 изменялось // пишет студент
-
+                            value={value1}
+                            onChange={change}
                         />
                     </div>
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
-                            // сделать так чтоб value1/2 изменялось // пишет студент
-
+                            value={[value1, value2]}
+                            onChange={change}
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
